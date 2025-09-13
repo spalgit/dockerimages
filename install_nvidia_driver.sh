45642c4b2d83bfe74cc7f05c49496654c6c457c9
sandeepbii@yahoo.com <spal@vm-gpu.us-central1-a.c.cheminfosolutions-prod-02.internal>
2025-09-13 20:09:43 +0000

    adding conda new env

--- install_nvidia_driver.sh
+++ install_nvidia_driver.sh
@@ -25,6 +25,7 @@ wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash Miniconda3-latest-Linux-x86_64.sh
 source ~/miniconda3/bin/activate
 conda init
+conda create --name my_pmx python=3.9
 chmod g+rx /home/spal
 sudo reboot
 
