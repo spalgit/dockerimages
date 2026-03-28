10eccd051743dab7a120d83e5c906a3a7f44f6dc
spalgit <sandeepbii@yahoo.com>
2026-03-28 19:16:44 +0000

    Cleaning up QSARTUNA regression script

--- QSARTuna/qsartuna_regression.py
+++ QSARTuna/qsartuna_regression.py
@@ -103,9 +103,10 @@ config = OptimizationConfig(
     settings=OptimizationConfig.Settings(
         mode=ModelMode.REGRESSION,
         n_splits=3,
-        n_trials=5,  # Total number of trials. Make sure to change to 100 or so for a production run
+        n_trials=100,  # Total number of trials. Make sure to change to 100 or so for a production run
         n_startup_trials=50,  # Number of startup ("random") trials.
         random_seed=42, # Seed for reproducability
+        scoring="r2",
         direction=OptimizationDirection.MAXIMIZATION,
     ),
 )
@@ -136,12 +137,13 @@ warnings.warn = warn
 
 study = optimize(config, study_name="my_study")
 
+plt.figure()
 sns.set_theme(style="darkgrid")
 default_reg_scoring= config.settings.scoring
 ax = sns.scatterplot(data=study.trials_dataframe(), x="number", y="value");
 ax.set(xlabel="Trial number", ylabel=f"Ojbective value\n({default_reg_scoring})");
 plt.savefig(f"{dirname}/optimization_progress.png", dpi=300, bbox_inches="tight")
-
+plt.close()
 
 
 cv_test = study.trials_dataframe()["user_attrs_test_scores"].map(lambda d: d[default_reg_scoring])
@@ -153,9 +155,12 @@ for i, vs in cv_test.items():
         x.append(i)
         y.append(v)
         fold.append(idx)
+
+plt.figure()
 ax = sns.scatterplot(x=x, y=y, hue=fold, style=fold, palette='Set1')
 ax.set(xlabel="Trial number", ylabel=f"Ojbective value\n({default_reg_scoring})");
 plt.savefig(f"{dirname}/CV_optimization_progress.png", dpi=300, bbox_inches="tight")
+plt.close()
 
 
 buildconfig = buildconfig_best(study)
@@ -190,6 +195,8 @@ rmse = np.sqrt(mean_squared_error(y_true=expected, y_pred=predicted))
 # MAE
 mae = mean_absolute_error(y_true=expected, y_pred=predicted)
 
+
+plt.figure()
 plt.scatter(expected, predicted)
 
 # Get the current axes (this is what you should call .plot on)
@@ -213,3 +220,7 @@ ax.text(
 )
 
 plt.savefig(f"{dirname}/Test_set_Regression_corr.png", dpi=300, bbox_inches="tight")
+plt.close()
+
+
+build_merged(buildconfig, f"{dirname}/merged.pkl")
